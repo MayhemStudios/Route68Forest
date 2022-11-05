d37e6ab8044893807f66c71ddc317b81a420f297
Mayhem Studios <55484272+MayhemStudios@users.noreply.github.com>
2022-11-06 05:23:04 +1000

    Delete Route68Forest directory

--- Route68Forest/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version "cerulean"
-author "MayhemStudios"
-game { 'gta5' }
-discription "Bring Route 68 to life!"
-repository "https://github.com/MayhemStudios/Route68Forest"
-version "1.0.0"
-
-this_is_a_map "yes"
