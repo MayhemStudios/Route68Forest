6f25f6578f736ccf832cded47e70aa21d88fec83
Mayhem Studios <55484272+MayhemStudios@users.noreply.github.com>
2022-11-05 12:23:37 -0700

    Add files via upload

---
+++ Route68Forest/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version "cerulean"
+games { 'gta5' }
+author "MayhemStudios"
+discription "Bring Route 68 to life!"
+repository "https://github.com/MayhemStudios/Route68Forest"
+version "1.0.0"
+
+this_is_a_map "yes"
